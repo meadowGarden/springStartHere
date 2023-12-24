9828cdac470d4cc5d489b14c33616017b6c5ce0a
dariusLenovoPC <jomantasdarius@yahoo.com>
2023-12-24 13:50:54 +0200

    Added initial files

---
+++ src/main/java/org/darruks/startHere/StartHereApplication.java
@@ -0,0 +1,13 @@
+package org.darruks.startHere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartHereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/darruks/startHere/StartHereApplicationTests.java
@@ -0,0 +1,13 @@
+package org.darruks.startHere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartHereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
